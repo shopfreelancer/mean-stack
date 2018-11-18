296d211a831eaf4b557c1d60c097dc2ed1764c00
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-11-18 12:14:23 +0100

    refactoring: changed create/add form to ReactiveFormsModule

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit } from '@angular/core';
 import { ParamMap } from '@angular/router';
 import {NgForm} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 import {PostsService} from '../posts.service';
 import {ActivatedRoute} from '@angular/router';
 import { Post } from '../post.model';
@@ -15,35 +16,48 @@ export class PostCreateComponent implements OnInit {
   private id: string;
   public post: Post;
   public isLoading = false;
+  public form: FormGroup;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute) {}
 
   onSavePost(form: NgForm) {
-    if (form.invalid) {
+    if (this.form.invalid) {
       return;
     }
     if(this.mode === 'edit'){
-      this.postsService.updatePost(this.id, form.value.title, form.value.content);
+      this.postsService.updatePost(this.id, this.form.value.title, this.form.value.content);
     } else {
-      this.postsService.addPost(form.value.title, form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     }
 
-    form.resetForm();
+    this.form.reset();
   }
 
   ngOnInit(){
+    this.form = new FormGroup({
+      'title': new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      'content': new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
      this.route.paramMap.subscribe((paramMap: ParamMap) => {
        if(paramMap.has('id')){
           this.mode = 'edit';
           this.id = paramMap.get('id');
           this.isLoading = true;
           this.postsService.getPost(this.id).subscribe(postData => {
+            this.isLoading = false;
             this.post = {
               id: postData._id,
               title: postData.title,
               content: postData.content
             };
-            this.isLoading = false;
+            this.form.setValue({
+              'title': this.post.title,
+              'content': this.post.content
+            });
           });
        } else {
          this.mode = 'create';
