ea653fc62d9fc5306d1f8b5c131da2fb691c2a8c
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-11-24 10:57:02 +0100

    added env variables for angular

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,12 @@
-import {Post} from './post.model';
-import {Subject} from 'rxjs';
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import {Router} from '@angular/router';
+import { Post } from './post.model';
+import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+
+const BACKEND_URL = environment.apiUrl + '/posts';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -19,7 +22,7 @@ export class PostsService {
   getPosts(postsPerPage: number, currentPage: number) {
     const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
     this.http
-      .get<{ message: string, posts: any, totalPosts: number }>('http://localhost:3000/api/posts' + queryParams)
+      .get<{ message: string, posts: any, totalPosts: number }>(BACKEND_URL + queryParams)
       .pipe(map(postData => {
         return {
           posts: postData.posts.map(post => {
@@ -49,7 +52,7 @@ export class PostsService {
     postData.append('title', title);
     postData.append('content', content);
     postData.append('image', image, title);
-    this.http.post<{ message: string, post: Post }>('http://localhost:3000/api/posts', postData)
+    this.http.post<{ message: string, post: Post }>(BACKEND_URL, postData)
       .subscribe(() => {
         this.router.navigate(['/']);
       });
@@ -67,7 +70,7 @@ export class PostsService {
       content: string,
       imagePath: string,
       userId: string
-    }>('http://localhost:3000/api/posts/' + id);
+    }>(BACKEND_URL + '/' + id);
   }
 
   updatePost(id: string, title: string, content: string, image: File | string) {
@@ -87,13 +90,13 @@ export class PostsService {
         userId: null // mongoose sets this automatically on backend
       };
     }
-    this.http.put('http://localhost:3000/api/posts/' + id, postData)
+    this.http.put(BACKEND_URL + '/' + id, postData)
       .subscribe(response => {
         this.router.navigate(['/']);
       });
   }
 
   deletePost(id: string) {
-    return this.http.delete('http://localhost:3000/api/posts/' + id);
+    return this.http.delete(BACKEND_URL + '/' + id);
   }
 }
