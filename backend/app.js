9a0c72b4f8ece906a521882118db1f1fd425258f
Hans-Christian Psaar <shopfreelancer@gmail.com>
2018-11-24 12:33:04 +0100

    changed app setup to deploy frontend/backend as one app with one url

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,10 @@ const usersRoutes = require('./routes/users');
 const uri = process.env.MONGO_CONNECTION;
 mongoose.connect(uri, { useNewUrlParser: true })
   .then(() => {
-    console.log("Connected to db");
+    console.log('Connected to db');
   })
   .catch(() => {
-    console.log("Error while connecting to DB");
+    console.log('Error while connecting to DB');
   });
 
 /**
@@ -31,17 +31,18 @@ mongoose.set('useCreateIndex', true);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/images', express.static(path.join('backend/images')));
+app.use('/', express.static(path.join(__dirname, 'angular')));
 
 /**
  * Set headers and handle CORS
  */
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
@@ -50,5 +51,9 @@ app.use((req, res, next) => {
  */
 app.use('/api/posts', postsRoutes);
 app.use('/api/users', usersRoutes);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, 'angular', 'index.html'));
+});
 
 module.exports = app;
